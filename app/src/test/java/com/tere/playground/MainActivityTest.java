cfae2b784b9db0ad6b5d0ccafa590dc819992fb9
TonyTandAndroid <tonythompsoncmu@gmail.com>
2022-01-30 19:18:44 -0800

    Add unit test.

--- app/src/test/java/com/tere/playground/MainActivityTest.java
+++ app/src/test/java/com/tere/playground/MainActivityTest.java
@@ -1,9 +1,18 @@
 package com.tere.playground;
 
+import com.google.common.truth.Truth;
+import org.junit.Test;
+
 /**
  * Created by Ahmed Adel Ismail on 8/1/2017.
  */
 public class MainActivityTest {
 
 
+  @Test
+  public void testIt() {
+
+    Truth.assertThat(1+1).isEqualTo("2");
+
+  }
 }

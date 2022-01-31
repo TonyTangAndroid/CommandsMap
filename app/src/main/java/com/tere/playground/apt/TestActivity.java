5026c4fa0a663b233b826a4f6b5e678f9e98805a
TonyTandAndroid <tonythompsoncmu@gmail.com>
2022-01-30 19:04:09 -0800

    Adjust consumer example code.

--- app/src/main/java/com/tere/playground/apt/TestActivity.java
+++ app/src/main/java/com/tere/playground/apt/TestActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import android.widget.TextView;
 import com.annotations.Command;
 import com.annotations.CommandsMapFactory;
 import com.mapper.CommandsMap;
@@ -20,6 +21,7 @@ public class TestActivity extends AppCompatActivity {
 
     private static final String TAG = TestActivity.class.getSimpleName();
     private CommandsMap commandsMap;
+    private TextView tv_text;
 
     public TestActivity() {
 
@@ -36,6 +38,7 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_text = findViewById(R.id.tv_text);
         commandsMap.execute("0");
         commandsMap.execute(1L, "test >>> 1");
         commandsMap.execute(2, "test >>> 2", "<<<<<");
@@ -44,16 +47,23 @@ public class TestActivity extends AppCompatActivity {
     @Command(keyString = "0")
     void printZero() {
         Log.d(TAG, "printZero()");
+        tv_text.append("printed Zero");
+        tv_text.append("\n");
     }
 
     @Command(keyLong = 1L)
     void printOne(String s) {
         Log.e(TAG, s);
+        tv_text.append(String.format("printed One:%s",s));
+        tv_text.append("\n");
+
     }
 
     @Command(2)
     void printTwo(String s1, String s2) {
         Log.e(TAG, s1 + s2);
+        tv_text.append(String.format("printed Two:%s + %s",s1, s2));
+        tv_text.append("\n");
     }
 
 
